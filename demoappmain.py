f1e639b8910b931d64b3e35f205677a1e903d285
Mihailo Bogdanovic <mihailo.bogdanovic1@gmail.com>
2021-11-22 16:13:10 +0100

    added initial files

---
+++ demoappmain.py
@@ -0,0 +1,18 @@
+from PyQt6 import QtWidgets
+import demoapp
+import sys
+
+class MainWindow(demoapp.Ui_MainWindow,QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    demo = MainWindow()
+    demo.show()
+    sys.exit(app.exec())
+
+
