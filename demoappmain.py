365ee5d60f25cac304c6d19cc5607ab2aae3214d
Mihailo Bogdanovic <mihailo.bogdanovic@avisto-eastern.com>
2021-11-23 10:46:06 +0100

    added widgets to customTab

--- demoappmain.py
+++ demoappmain.py
@@ -1,4 +1,5 @@
 from PyQt6 import QtWidgets
+from PyQt6.QtCore import Qt
 import demoapp
 import sys
 
@@ -6,14 +7,43 @@ class customTab(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
     
-    tablayout = QtWidgets.QVBoxLayout()
+        self.createWidgets()        
+        self.addWidgetsToLayout()
+
+    def createWidgets(self):
+        self.tablayout = QtWidgets.QGridLayout(self)
+        self.textOutput = QtWidgets.QTextEdit()
+        self.textInput = QtWidgets.QLineEdit()
+        self.clearButton = QtWidgets.QPushButton()
+        self.clearButton.setText("Clear")
+        self.dropDownMenu = QtWidgets.QComboBox()
+        self.dropDownMenu.addItem("temp")
+        self.checkBox = QtWidgets.QCheckBox()
+        self.checkBox.setText("temp")
+
+    def addWidgetsToLayout(self):
+        self.tablayout.addWidget(self.textOutput,0,0,1,2)
+        self.tablayout.addWidget(self.clearButton,1,0)
+        self.tablayout.addWidget(self.textInput,1,1)
+        self.tablayout.addWidget(self.dropDownMenu,2,0)
+        self.tablayout.addWidget(self.checkBox,2,1)    
+    
+        #self.tablayout.addWidget(self.textOutput)
+        #self.tablayout.addWidget(self.textInput)
+        #self.tablayout.addWidget(self.clearButton)
+        #self.tablayout.addWidget(self.dropDownMenu)
+        #self.tablayout.addWidget(self.checkBox) 
+
+        
+
+    
 
 class MainWindow(demoapp.Ui_MainWindow,QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
-        self.tabWidget.removeTab(1)
+        self.tabWidget.removeTab(1) #remove the 2 default tabs from qt designer
         self.tabWidget.removeTab(0)
 
         tab0 = customTab()
